ee2cbcc9287732ae85291859bc69dc7e2cd772d1
vargapeter66 <varga.peter.mobil@gmail.com>
2021-11-11 11:38:53 +0100

    v1, test ok

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,12 @@
+const removeDuplicatesFromArray = (arr1 = [0]) => {
+  const arr2 = arr1.filter((element, index) => {
+    return arr1.indexOf(element) === index;
+});
+return arr2
+}
 
 
 
 
 
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,15 @@
 
 
+const HU = {
+  date(date)  { return new Intl.DateTimeFormat('hu-HU').format(date) }, 
+  curreny(price = 0) { return new Intl.NumberFormat('hu-HU', {style: 'currency', currency: 'HUF'}).format(price) },
+  list(array = ['első', 'második', 'harmadik']) { return `${array[0]}, ${array[1]} és ${array[2]}` }
+  }
 
 
+// console.log(HU.date(new Date("2021-04-01")));
+// console.log(HU.curreny(50));
+// console.log(HU.list(['1', '2', '3']));
 
 
-// export default HU
+export default HU;
